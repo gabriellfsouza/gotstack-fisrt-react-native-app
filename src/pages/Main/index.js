f6a24105b6493711569004ae7b2fd95f52781227
Gabriel Lima <gabriellfsouza@gmail.com>
2019-07-20 11:48:01 -0300

    feature: github info add list and styling

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default class Main extends Component {
   state = {
@@ -19,7 +31,7 @@ export default class Main extends Component {
       name: response.data.name,
       login: response.data.login,
       bio: response.data.bio,
-      avatar: response.data.avatar,
+      avatar: response.data.avatar_url,
     };
 
     this.setState({
@@ -36,7 +48,7 @@ export default class Main extends Component {
       <Form>
         <Input
           autoCorrect={false}
-          autoCapitalizer="none"
+          autoCapitalize="none"
           placeholder="Adicionar usuário"
           value={newUser}
           onChangeText={text => this.setState({ newUser: text })}
@@ -47,6 +59,22 @@ export default class Main extends Component {
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
+
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{uri: item.avatar}} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+
+            <ProfileButton onPress={() => {}} >
+              <ProfileButtonText>Ver perfil</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
     </Container>
     );
   }
