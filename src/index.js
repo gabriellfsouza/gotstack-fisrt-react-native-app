467f50c9d4a13025aef6b2db4398ddf31138d9f6
Gabriel Lima <gabriellfsouza@gmail.com>
2019-07-20 09:53:26 -0300

    conf: reactotron lib included.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+import './config/ReactotronConfig';
+
+console.tron.log('teste');
+
+const App = () => {
+  return (
+    <View>
+      <Text>Engine: Hermes</Text>
+    </View>
+  );
+};
+
+
+export default App;
